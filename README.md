8adc42f05f13f3f63366e8bd9cde01468149320d
Martin-MacDonald <mmacdo54@caledonian.ac.uk>
2017-10-16 11:20:13 +0100

    Fully working basic game

---
+++ README.md
@@ -0,0 +1,14 @@
+# Bingo Game SImulator
+
+- To run download the BingoGameSim.jar file
+- Either just click to run
+- Or you can run from the command line using: java -jar BingoGameSim.jar
+
+# How it works
+
+- On launch you will get assigned 16 numbers as well as two other rival players who each have a random 16 numbers selected
+- Simply click the button to generate a new number
+- If it is in our list it will remove it, as it will for your rivals list
+- Keep drawing balls until there is a winner
+- You can reset the game and keep playing
+- It will keep a tally of your win rate at the bottom
--- src/BingoGameSim.java
+++ src/BingoGameSim.java
@@ -12,6 +12,9 @@ import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import javax.swing.ButtonModel;
 import java.awt.event.ActionEvent;
+import java.awt.Font;
+import javax.swing.JComponent;
+import javax.swing.Box;
 
 public class BingoGameSim extends JFrame{
 	
@@ -25,9 +28,12 @@ public class BingoGameSim extends JFrame{
 	private JLabel player3Label;
 	private JLabel ballLabel;
 	private JLabel gameOverLabel;
+	private boolean gameInProgress;
+	private JLabel scoreLabel;
 	
 	public BingoGameSim(){
 		
+		gameInProgress = true;
 		createPlayers();
 		createBalls();
 		initComponents();
@@ -55,7 +61,9 @@ public class BingoGameSim extends JFrame{
 		JPanel mainPanel = new JPanel();
 		mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
 		
-		ballLabel = new JLabel("Next Ball: XX");
+		scoreLabel = new JLabel("Win rate: 0/0");
+		
+		ballLabel = new JLabel("Ball: XX");
 		
 		nextBallButton = new JButton("Start");
 		ButtonModel nextBallButtonModel = nextBallButton.getModel();
@@ -82,14 +90,35 @@ public class BingoGameSim extends JFrame{
 		gameOverLabel = new JLabel("You win");
 		gameOverLabel.setVisible(false);
 		
+		setComponentFontAndAlign(scoreLabel, ballLabel, nextBallButton, player1Label, player2Label, player3Label, gameOverLabel);
+		
 		mainPanel.add(ballLabel);
+		mainPanel.add(Box.createVerticalStrut(20));
 		mainPanel.add(nextBallButton);
+		mainPanel.add(Box.createVerticalStrut(20));
 		mainPanel.add(player1Label);
+		mainPanel.add(Box.createVerticalStrut(20));
 		mainPanel.add(player2Label);
+		mainPanel.add(Box.createVerticalStrut(20));
 		mainPanel.add(player3Label);
+		mainPanel.add(Box.createVerticalStrut(20));
 		mainPanel.add(gameOverLabel);
+		mainPanel.add(Box.createVerticalStrut(20));
+		mainPanel.add(scoreLabel);
+		mainPanel.add(Box.createVerticalStrut(20));
 		add(mainPanel);
-		pack();
+		setSize(1500, 600);
+		
+	}
+	
+	private void setComponentFontAndAlign(JComponent... args){
+		
+		Font font = new Font("Serif", Font.BOLD, 30);
+		
+		for (JComponent arg : args){
+			arg.setFont(font);
+			arg.setAlignmentX(JComponent.CENTER_ALIGNMENT);
+		}
 		
 	}
 	
@@ -120,24 +149,62 @@ public class BingoGameSim extends JFrame{
 		nextBallButton.setText("Next Ball");
 		
 		if (bp1.isBoardComplete() || bp2.isBoardComplete() || bp3.isBoardComplete()){
+			if (bp1.isBoardComplete()){
+				bp1.setGameTally(true);
+				gameOverLabel.setText("YOU WON!!!!");
+			}else{
+				bp1.setGameTally(false);
+				gameOverLabel.setText("SORRY. BETTER LUCK NEXT TIME!!!");
+			}
+			
+			
+			gameInProgress = false;
+			changeGameState();			
+		}
+		
+		updateBalls();		
+		
+	}
+	
+	private void updateBalls(){
+		
+		player1Label.setText("You have the remaining balls: " + bp1.getBoardNumbers());
+		player2Label.setText("Player 2 has the remaining balls: " + bp2.getBoardNumbers());
+		player3Label.setText("Player 3 has the remaining balls: " + bp3.getBoardNumbers());
+		
+	}
+	
+	private void changeGameState(){
+		
+		if (!gameInProgress){
+			
 			ballLabel.setVisible(false);
 			nextBallButton.setText("Play again?");
 			player1Label.setVisible(false);
 			player2Label.setVisible(false);
 			player3Label.setVisible(false);
 			gameOverLabel.setVisible(true);
+			scoreLabel.setVisible(false);
+			
+		} else {
+			
+			ballLabel.setVisible(true);
+			ballLabel.setText("Ball: XX");
+			nextBallButton.setText("Start");
+			player1Label.setVisible(true);
+			player2Label.setVisible(true);
+			player3Label.setVisible(true);
+			gameOverLabel.setVisible(false);
+			scoreLabel.setVisible(true);
+			scoreLabel.setText("Win rate: " + bp1.getNumOfWins() + "/" + bp1.getGamesCompleted());
 			
 		}
-		
-		player1Label.setText("You have the remaining balls: " + bp1.getBoardNumbers());
-		player2Label.setText("Player 2 has the remaining balls: " + bp2.getBoardNumbers());
-		player3Label.setText("Player 3 has the remaining balls: " + bp3.getBoardNumbers());
-		pack();
-		
 		
 	}
 	
 	private void resetGame(){
+		
+		gameInProgress = true;
 		
 		bb.resetBalls();
 		
@@ -145,21 +212,10 @@ public class BingoGameSim extends JFrame{
 		bp2.playAgain();
 		bp3.playAgain();
 		
-		player1Label.setText("You have the remaining balls: " + bp1.getBoardNumbers());
-		player2Label.setText("Player 2 has the remaining balls: " + bp2.getBoardNumbers());
-		player3Label.setText("Player 3 has the remaining balls: " + bp3.getBoardNumbers());
+		updateBalls();
 		
-		ballLabel.setVisible(true);
-		ballLabel.setText("Next Ball: XX");
-		nextBallButton.setText("Start");
-		player1Label.setVisible(true);
-		player2Label.setVisible(true);
-		player3Label.setVisible(true);
-		gameOverLabel.setVisible(false);
-		
-		pack();
-		
-		System.out.println("Start new game");
+		changeGameState();
+
 	}
 	
 }
